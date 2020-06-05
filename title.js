710d75c7f81451da64ae486821fcc3b15de15436
Alex N <alexiscool745@gmail.com>
2020-06-05 19:16:36 +0300

    Create title.js
    the main thing

---
+++ title.js
@@ -0,0 +1,25 @@
+if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", function() {
+        loaded();
+    });
+} else if (document.attachEvent) {
+    document.attachEvent("onreadystatechange", function() {
+        loaded();
+    });
+}
+
+function loaded() {
+
+    setInterval(loop, 400);
+
+}
+
+var x = 0;
+
+var titleText = [ "Java", "Script", "JavaScript"];
+
+function loop() {
+
+    document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+
+}
